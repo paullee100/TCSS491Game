3ab4b133efebff232c1e142518214ceef633de8f
Dylan Frederick <dylanf47@uw.edu>
2023-01-17 14:34:28 -0800

    updated some animations, some animations no longer cancel when letting go of key

--- knight.js
+++ knight.js
@@ -20,12 +20,12 @@ class Knight {
         this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/Knight_Roll.png"));
 
         //spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop
-        this.animation.push(new Animator(this.spritesheet[0], 43, 41, 30, 40, 10, 0.1, 90, false, true));
+        this.animation.push(new Animator(this.spritesheet[0], 43, 41, 30, 40, 10, 0.060, 90, false, true));
         this.animation.push(new Animator(this.spritesheet[1], 37, 37, 85, 45, 4, 0.1, 35, false, true));
         this.animation.push(new Animator(this.spritesheet[2], 30, 40, 88, 40, 6, 0.1, 32, false, true));
         this.animation.push(new Animator(this.spritesheet[3], 45, 43, 20, 36, 10, 0.1, 100, false, true));
-        this.animation.push(new Animator(this.spritesheet[4], 42, 41, 42, 37, 12, 0.1, 78, false, true));
-        this.animation.push(new Animator(this.spritesheet[5], 43, 41, 30, 40, 5, 0.1, 90, false, true));
+        this.animation.push(new Animator(this.spritesheet[4], 42, 41, 42, 37, 12, 0.075, 78, false, true));
+        this.animation.push(new Animator(this.spritesheet[5], 43, 41, 30, 40, 12, 0.075, 90, false, true));
     
         this.readyToAttack = 0;
         this.updateBB();
@@ -52,7 +52,7 @@ class Knight {
 
         const TICK = this.game.clockTick;
 
-        if (this.state != 5) {
+        if (this.state != 5 && this.state != 4 && this.state != 1 && this.state != 2) {
             if (this.game.keys["a"] || this.game.keys["ArrowLeft"]) {
                 console.log("A is pressed");
                 this.facing = -1;
@@ -79,8 +79,10 @@ class Knight {
                 this.state = 1;
                 this.updateBB();
 
-            } else if (this.game.keys["Shift"]) {
+            } else if (this.game.keys["Shift"] || (this.game.keys["Shift"] && (this.game.keys["a"] || this.game.keys["d"]))) {
                 this.state = 4;
+                this.velocity.x = 500 * (this.facing);
+                this.spritesheet[this.state].ad
             } else {
                 this.state = 3;
                 this.velocity.x = 0;
@@ -126,7 +128,7 @@ class Knight {
             //mid-air physics
             //vertical physics
             if (this.velocity.y < 0 && this.game.keys["w"]) { // holding A while jumping jumps higher
-                //this.velocity.y -= 1000;
+                //this.velocity.y -= 25;
             };
 
             // horizontal physics
@@ -140,7 +142,9 @@ class Knight {
 
         };
 
-        this.velocity.y += FALL * TICK;
+        if (this.position.y < 540) {
+            this.velocity.y += FALL * TICK;
+        }
 
         this.position.x += this.velocity.x * TICK;
         this.position.y += this.velocity.y * TICK;
@@ -185,6 +189,7 @@ class Knight {
         var stateMod = 0;
         if(this.state == 0) stateMod = 20;
         else if (this.state == 1) stateMod = 100;
+        else if (this.state == 4) stateMod = 50;
 
         if(this.facing == 1) this.animation[this.state].drawFrame(this.game.clockTick, ctx, (this.position.x - stateMod), this.position.y, 5);
         else this.animation[this.state].drawFrame(this.game.clockTick, ctx, (this.position.x * this.facing) - 100 + (stateMod * this.facing), this.position.y, 5);
