71ced6cf2a7885227de22733c4eb5a8018eabc7a
paullee100 <100502446+paullee100@users.noreply.github.com>
2023-02-02 17:11:07 -0800

    Update scenemanager.js

--- scenemanager.js
+++ scenemanager.js
@@ -5,7 +5,7 @@ class SceneManager {
         this.knight = new Knight(game);
         this.x = 0;
         this.y = 0;
-        this.loadLevel(1, 9 * PARAMS.BLOCKWIDTH, 8.49609375 * PARAMS.BLOCKWIDTH, false, true);
+        this.loadLevel(1, 9 * PARAMS.BLOCKWIDTH, 6 * PARAMS.BLOCKWIDTH, false, true);
 
 
 
@@ -70,31 +70,6 @@ class SceneManager {
         this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles27.png", 48*PARAMS.BLOCKWIDTH, 11 * PARAMS.BLOCKWIDTH ,64,64));
         this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles27.png", 49*PARAMS.BLOCKWIDTH, 11 * PARAMS.BLOCKWIDTH ,64,64));
         this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles28.png", 50*PARAMS.BLOCKWIDTH, 11 * PARAMS.BLOCKWIDTH ,64,64));
-        
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles00.png", -5 - this.x, 715 - this.y ,64, 64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles00.png", -5- this.x, 715- this.y ,64, 64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 59- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 123- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 187- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 251- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 315- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 379- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 379- this.x, 779- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 443- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 443- this.x, 651- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 507- this.x, 651 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 507- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 571- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 635- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 699- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 763- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 827- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 891- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 955- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 1019- this.x, 715- this.y ,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 1083- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles01.png", 1147- this.x, 715 - this.y,64,64));
-        this.game.addEntity(new Tile(this.game, "./tileset/forest/forest_tiles02.png", 1211- this.x, 715- this.y ,64,64));
 
         this.game.addEntity(new Background3());
         this.game.addEntity(new Background2());
